55a6ba337e34c78b4564958ee76a2affbeebc4c6
osman <osman@example.com>
2022-03-26 00:52:19 +0200

    composer update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,53 +1,16 @@
 <?php
 
-namespace App\Exceptions;
+namespace App\Exceptions;use Throwable;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Throwable  $exception
-     * @return void
-     *
-     * @throws \Throwable
-     */
+   // use Throwable - you should NOT import Throwable class as a trait here. You need to just import it above the class    
     public function report(Throwable $exception)
     {
         parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Throwable
-     */
+    }    
     public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
